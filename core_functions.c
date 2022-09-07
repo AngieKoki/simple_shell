531ceebc2b8f33ccfe31757e756f7e5776494fd5
Bradley Kibs <Bradleykibwana@gmail.com>
2022-09-07 16:21:56 +0300

    merge conflicts

--- core_functions.c
+++ core_functions.c
@@ -134,32 +134,3 @@ int execute(char **arg, char **argv, int counter)
 	free(buffer);
 	return (status_output);
 }
-/**
- * _which - search ofr a command in the directories
- * @head: head
- * @av: arguments
- * Return: path of a command or nil
- **/
-char *_which(link_t **head, char *av)
-{
-	link_t *pusher = *head;
-	char *buffer;
-
-	if (av[0] == '.' || av[0] == '/')
-	{
-		if (access(av, X_OK) == 0)
-			return (av);
-	}
-
-	while (pusher)
-	{
-		buffer = _strcat(pusher->dir, "/", av);
-		if (access(buffer, X_OK) == 0)
-		{
-			return (buffer);
-		}
-		free(buffer);
-		pusher = pusher->next;
-	}
-	return (NULL);
-}
